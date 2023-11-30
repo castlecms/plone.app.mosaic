adecc384a93194666ad06f1bc2f360538956efff
Joel Kleier <joel.kleier@wildcardcorp.com>
2023-11-30 08:10:30 -0600

    Preparing release 2.2.5+castle1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.2.5.dev0+castle1'
+version = '2.2.5+castle1'
 
 long_description = (
     open('README.rst').read() + '\n' +
